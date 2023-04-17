1aee9bc9958a68940ab90137457a24d2dee19dac
st-Helicoprion <christiankwadranck30@gmail.com>
2023-04-17 16:40:40 +0800

    Added player movement

---
+++ Assets/Scripts/CharMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMove : MonoBehaviour
+{
+    public Rigidbody playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("A"))
+        {
+            playerRB.velocity += Vector3.forward*.5f;
+        }
+    }
+}
