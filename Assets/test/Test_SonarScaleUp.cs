7ac75e2c148eca6f4a6c6c01b25ffcd072aaada8
HurryPeter <96000131+HurryPeter@users.noreply.github.com>
2023-05-21 21:55:34 +0800

    無所謂的更動，測試聲波形狀為cone到一半

--- Assets/test/Test_SonarScaleUp.cs
+++ Assets/test/Test_SonarScaleUp.cs
@@ -6,12 +6,14 @@ public class Test_SonarScaleUp : MonoBehaviour
 {
     public GameObject prefab;
     public Transform point;
+    Test_SonarBehavior test_SonarBehavior;
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            Instantiate(prefab,point.position,Quaternion.identity);
+            test_SonarBehavior = prefab.GetComponent<Test_SonarBehavior>();
+            Instantiate(prefab, transform.position - new Vector3(0, test_SonarBehavior.SonarScale, 0), Quaternion.identity);
         }
     }
-    
+
 }
